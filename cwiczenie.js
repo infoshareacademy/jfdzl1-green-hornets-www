1f368da6c00c867441d46b7757e8cce45adcfbcb
Maciej Gornicki <mgornicki.grafik@gmail.com>
2017-09-17 14:01:40 +0200

    Added changes i JS code

--- cwiczenie.js
+++ cwiczenie.js
@@ -1,11 +1,11 @@
-for(var i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {console.log("FooBuzz")}
-    else if (i % 3 === 0) {
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FooBuzz");
+    } else if (i % 3 === 0) {
         console.log("Foo");
-    }
-    else if (i % 5 === 0) {
+    } else if (i % 5 === 0) {
         console.log("Buzz");
     } else {
-        console.log(i)
+        console.log(i);
     }
 }
