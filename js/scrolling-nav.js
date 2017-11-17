68104483c250e1a88e491e8545249856331482a6
Darqoo <d.smolag@gmail.com>
2017-11-17 20:53:22 +0100

    fixed smothly scroll

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,29 +1,23 @@
 (function($) {
-  "use strict"; // Start of use strict
+    "use strict";
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+    var navOffset = $('.navbar').height();
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[target*="#"]:not([target="#"])').click(function() {
+        var target = $(this.target);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            var offset_top = target.offset().top - navOffset - 24;
+            $('body').animate({
+                scrollTop: (offset_top)
+            }, 1000, "easeInOutExpo");
+            return false;
+        }
+    });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').removeClass('show');
+        $('.navbar-collapse').addClass('hide');
+    });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
-})(jQuery); // End of use strict
+})(jQuery);
