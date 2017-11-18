0d5af5d937ab5239c8565d78477c05a3f3777e7c
darqoo <d.smolag@gmail.com>
2017-11-18 13:46:34 +0100

    added cookies

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+(function() {
+if (localStorage.acceptCookies) {
+$('.cookies').hide();
+}
+})();
+
+
+
 var onAcceptClick = function ()
 {
     $('.cookies').css('display', 'none')
     $('.try--fixed').css('bottom', '45px');
     event.preventDefault();
+    var acceptCookies = { 'accept': "true" };
+    localStorage.setItem('acceptCookies', acceptCookies.accept);
 }
